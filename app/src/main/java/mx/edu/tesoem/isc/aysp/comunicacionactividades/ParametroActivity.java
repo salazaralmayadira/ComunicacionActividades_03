6351be7a16ea5db58be045ab751ff86f15c20f95
salazaralmayadira <64059447+salazaralmayadira@users.noreply.github.com>
2020-04-26 22:26:20 -0500

    Ejercicio: comunicaciónn entre actividades

---
+++ app/src/main/java/mx/edu/tesoem/isc/aysp/comunicacionactividades/ParametroActivity.java
@@ -0,0 +1,22 @@
+package mx.edu.tesoem.isc.aysp.comunicacionactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ParametroActivity extends AppCompatActivity {
+    TextView lblsaludo;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametro);
+        lblsaludo= findViewById(R.id.lblsaludo);
+        Bundle parametro = getIntent().getExtras();
+        lblsaludo.setText("Bienvenido" + parametro.getString("Nombre"));
+
+
+    }
+}
